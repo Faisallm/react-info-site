33805a30a97efad103f0f04d3dc23451f643b33b
unknown <Faisallawan1997@gmail.com>
2023-08-15 02:21:03 -0700

    coder coder fruit

---
+++ index.js
@@ -0,0 +1,27 @@
+
+
+// const page = (
+//     <div>
+//         <h1 className="header">Faisal Muhammad</h1>
+//         <p>This is the single most important sentence.</p>
+//     </div>
+// );
+
+// console.log(page);
+
+
+// ReactDOM.render(page, document.getElementById('root'));
+
+
+const navigationBar = (
+    <nav className='nav-bar'>
+        <h1>GapTooth</h1>
+        <ul>
+            <li>Pricing</li>
+            <li>About</li>
+            <li>Contact</li>
+        </ul>
+    </nav>
+)
+
+ReactDOM.render(navigationBar, document.getElementById('root'))
